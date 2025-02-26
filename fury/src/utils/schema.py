0543ebed5ee3f819627c3678c41754b4a88b44f7
nyel-101 <dandevjr@gmail.com>
2025-02-26 12:21:47 +0000

    style(core): implement middleware routes

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2216454005c84497a78776ac678bdf87 = () => {
-  return '2026-03-18T22:12:10Z';
+const node_3eba5c39bc844ec19edc4c6f4db7ebf2 = () => {
+  return '2025-02-26T12:21:47Z';
 };
