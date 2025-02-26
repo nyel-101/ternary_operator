2e133c16fe0df36a696c8a38cc68499ba6e232f5
nyel-101 <dandevjr@gmail.com>
2025-02-26 09:30:35 +0000

    style(worker): sanitize caching layer

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_003ddd6ca80a48b388c28b28dca17b76 = () => {
-  return '2026-04-14T15:16:27Z';
+const node_2bb226c75e4a4ad3881734cf42017a64 = () => {
+  return '2025-02-26T09:30:35Z';
 };
