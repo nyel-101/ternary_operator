397a67374b9fc3bbfc2cc0f2db5a9ba623e08a50
nyel-101 <dandevjr@gmail.com>
2025-03-05 18:58:19 +0000

    fix(auth): sanitize payload parser

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8338fd434cdf4df4a3e15356818295bd = () => {
-  return '2026-03-03T16:55:23Z';
+const node_409f92cb69874fa88b45211471170f13 = () => {
+  return '2025-03-05T18:58:19Z';
 };
