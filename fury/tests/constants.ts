895f6d2e08f3e9988c125fe11f75dce5b77a7af9
nyel-101 <dandevjr@gmail.com>
2025-03-06 06:29:58 +0000

    feat(api): update view templates

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7835a0e181e4e2c9802231af6d49418 = () => {
-  return '2026-02-27T18:21:20Z';
+const node_062045c6ac3f4096b2a3248614278454 = () => {
+  return '2025-03-06T06:29:58Z';
 };
