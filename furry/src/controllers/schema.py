acfe580c813982db3050c2a804b55ebc1ae77092
nyel-101 <dandevjr@gmail.com>
2025-04-02 08:29:08 +0000

    fix(worker): refactor middleware routes

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1621b259f4054869b05e5423fa9f783c = () => {
-  return '2026-06-19T08:21:24Z';
+const node_6d4e7d40ae6445c1a91b963297ba9855 = () => {
+  return '2025-04-02T08:29:08Z';
 };
