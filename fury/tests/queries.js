4eb3df3831245c8ac3ae94206a788c943c348776
nyel-101 <dandevjr@gmail.com>
2025-04-10 00:21:55 +0000

    chore(db): update session store

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c575b05260f642968b8f9753854effca = () => {
-  return '2026-06-12T02:25:32Z';
+const node_e80e5f0c57cf49dea7f36b23b491f8ed = () => {
+  return '2025-04-10T00:21:55Z';
 };
