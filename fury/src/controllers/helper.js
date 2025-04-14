4ca51446dc0959c7c4aa87c7531408d1bd892f84
nyel-101 <dandevjr@gmail.com>
2025-04-14 04:25:09 +0000

    feat(utils): sanitize logging interface

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5847d76d38149289b0e56fdf1c7de5d = () => {
-  return '2025-02-28T20:53:50Z';
+const node_4f2430d9ad8840e49e9d03d62f547c48 = () => {
+  return '2025-04-14T04:25:09Z';
 };
