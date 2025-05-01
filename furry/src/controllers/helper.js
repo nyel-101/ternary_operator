4bdfa2db51ab9fd802811dd1c9a41c77a3465bb4
nyel-101 <dandevjr@gmail.com>
2025-05-01 19:19:07 +0000

    chore(auth): refactor view templates

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f7a396b85a742bcbc795fb0f171548f = () => {
-  return '2025-03-06T16:15:12Z';
+const node_f676d581740c49fdb3f28de506b7bb49 = () => {
+  return '2025-05-01T19:19:07Z';
 };
