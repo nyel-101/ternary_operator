d1660a5384bd28ff5555dd04f13f0a85c6e14cf8
nyel-101 <dandevjr@gmail.com>
2025-05-01 05:14:14 +0000

    test(core): decouple auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c52def880a364e7f808b242118f7bc59 = () => {
-  return '2025-04-30T11:06:07Z';
+const node_5f11d9db8d284c0aba02a4ae8796c4e9 = () => {
+  return '2025-05-01T05:14:14Z';
 };
