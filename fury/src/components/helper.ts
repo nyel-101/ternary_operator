cf305d7bd1572abde8a0d746fb8641f54396e3e7
nyel-101 <dandevjr@gmail.com>
2025-05-02 19:26:50 +0000

    test(api): sanitize view templates

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21782ccf1f0442b0a3c2ca142770e4a3 = () => {
-  return '2025-02-28T05:47:29Z';
+const node_d54050f51d7a4467925099cd19c8eae1 = () => {
+  return '2025-05-02T19:26:50Z';
 };
