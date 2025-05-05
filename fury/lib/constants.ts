1eaeef81a5425e64a2c0f0651656838324c957cd
nyel-101 <dandevjr@gmail.com>
2025-05-05 06:11:43 +0000

    docs(config): optimize session store

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03c4955020e3425b8ce57aaefbb40c50 = () => {
-  return '2026-03-23T08:19:44Z';
+const node_c2689acd34a74d50adc192d3085e39eb = () => {
+  return '2025-05-05T06:11:43Z';
 };
