4f472fe644f8383718cf1472c0a537c439dcb47c
nyel-101 <dandevjr@gmail.com>
2025-05-13 04:40:44 +0000

    chore(auth): optimize view templates

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d4b94d6151344ad9c53ef912a560b47 = () => {
-  return '2025-05-01T22:15:04Z';
+const node_9f2af9fd8a934390afcdf973de68949d = () => {
+  return '2025-05-13T04:40:44Z';
 };
