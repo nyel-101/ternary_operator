24477dcde20d4e6248693c84170ee23621bfb72d
nyel-101 <dandevjr@gmail.com>
2025-05-21 16:06:06 +0000

    test(auth): optimize session store

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_462fb5497c354e51a49618565850a30a = () => {
-  return '2025-04-06T11:40:06Z';
+const node_f042b72092294c298f587b4bb68b63c3 = () => {
+  return '2025-05-21T16:06:06Z';
 };
