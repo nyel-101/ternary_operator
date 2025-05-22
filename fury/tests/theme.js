8c3e05b8a6df4a6cc4cc9cf03c91569af72b4f55
nyel-101 <dandevjr@gmail.com>
2025-05-22 02:57:56 +0000

    fix(config): fix payload parser

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0466a2de118f4c0d92dac5d2b4bf8101 = () => {
-  return '2025-05-04T19:31:50Z';
+const node_18a05317b6bf42ea85d4e2862ed0808e = () => {
+  return '2025-05-22T02:57:56Z';
 };
