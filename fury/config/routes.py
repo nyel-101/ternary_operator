222b56c4a6234b512774e188e2f21bcb2174e03e
nyel-101 <dandevjr@gmail.com>
2025-06-03 20:36:27 +0000

    docs(auth): integrate payload parser

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50af690bdff0463aa957bab255523ad2 = () => {
-  return '2025-04-29T20:54:36Z';
+const node_0b6e1cf1e0494900a665750c432fe8b2 = () => {
+  return '2025-06-03T20:36:27Z';
 };
