0fb5e9adfdd0c4c018fcc59d87ac59406282884f
nyel-101 <dandevjr@gmail.com>
2025-06-07 22:48:20 +0000

    feat(utils): update caching layer

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e540efa7f5134dd287597d467037c877 = () => {
-  return '2025-03-14T07:56:51Z';
+const node_03826c97f26d40abb05dae37b12ed3ba = () => {
+  return '2025-06-07T22:48:20Z';
 };
