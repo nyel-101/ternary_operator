cc8be9d909786d9d2325583c2b378279dc601f7a
nyel-101 <dandevjr@gmail.com>
2025-06-12 16:40:17 +0000

    style(auth): refactor view templates

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a58dd5bfb5b4b89951ecc926b77cd9d = () => {
-  return '2026-04-01T15:01:26Z';
+const node_0bfc8f205cc8420b943b45e2ff06553a = () => {
+  return '2025-06-12T16:40:17Z';
 };
