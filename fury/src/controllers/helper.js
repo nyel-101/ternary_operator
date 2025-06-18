ed3097520c2e420d484010adfc49ffa651a5db92
nyel-101 <dandevjr@gmail.com>
2025-06-18 00:02:07 +0000

    feat(utils): fix caching layer

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f2430d9ad8840e49e9d03d62f547c48 = () => {
-  return '2025-04-14T04:25:09Z';
+const node_b009d5dd4866415ea5467028baf2fe89 = () => {
+  return '2025-06-18T00:02:07Z';
 };
