7dc23df2471b1955bb410f9c7e5f12a344027e89
nyel-101 <dandevjr@gmail.com>
2025-06-23 23:31:34 +0000

    perf(api): restructure caching layer

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c47ab81e40e541d3893df7266359fa3d = () => {
-  return '2025-03-03T07:53:53Z';
+const node_34f8f455fc2947febb2f23eb57bb30df = () => {
+  return '2025-06-23T23:31:34Z';
 };
