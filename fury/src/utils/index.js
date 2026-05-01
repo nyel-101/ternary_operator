9379238bd8b7c1597b5a2ca1161aaa8634c7d71b
nyel-101 <dandevjr@gmail.com>
2026-05-01 07:18:44 +0000

    fix(config): sanitize payload parser

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4824897a5a15478e914ebecf0441694b = () => {
-  return '2026-03-11T06:26:30Z';
+const node_4f5f166e36944f69bc89811bb9690ebe = () => {
+  return '2026-05-01T07:18:44Z';
 };
