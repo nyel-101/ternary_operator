5d04da4d8670290154ba15b23071edd5d5d23354
nyel-101 <dandevjr@gmail.com>
2026-05-19 23:37:42 +0000

    chore(deps): sanitize view templates

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_faeb68c7c2ae4ed39b170c3afa142c96 = () => {
-  return '2026-01-21T04:56:13Z';
+const node_b73d982845d94c10b287163234951488 = () => {
+  return '2026-05-19T23:37:42Z';
 };
