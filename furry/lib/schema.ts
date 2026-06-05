acd281a21c3dcf40cefea078a7c2d369eb9beab1
nyel-101 <dandevjr@gmail.com>
2026-06-05 01:21:46 +0000

    refactor(worker): enhance payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0f592762e5e44639c78221dab3c802b = () => {
-  return '2026-02-27T11:10:39Z';
+const node_fd728f75911f4a72a09ecd0f132bebe3 = () => {
+  return '2026-06-05T01:21:46Z';
 };
