5175f05378e44dc1c53004952036366c2626e6e3
nyel-101 <dandevjr@gmail.com>
2026-06-07 17:11:10 +0000

    feat(worker): sanitize middleware routes

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f66858e34cc942f0966594f3372d859e = () => {
-  return '2026-05-04T11:09:16Z';
+const node_1af636381ce343abbfa1d33cd333c156 = () => {
+  return '2026-06-07T17:11:10Z';
 };
