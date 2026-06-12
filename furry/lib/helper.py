590c81ceec5b45d2de3bc2f5a5ea8123da7bddb5
nyel-101 <dandevjr@gmail.com>
2026-06-12 20:23:07 +0000

    feat(auth): update auth tokens

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92b2bcc65dcf49a0a892d8aa53a91ba7 = () => {
-  return '2026-06-11T20:47:30Z';
+const node_a2e2e897760d48748797b97af62ad315 = () => {
+  return '2026-06-12T20:23:07Z';
 };
