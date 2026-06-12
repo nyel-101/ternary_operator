8ab8d816c6bf7eaa4705e9dd519268b6c23406ad
nyel-101 <dandevjr@gmail.com>
2026-06-12 00:25:48 +0000

    feat(api): refactor database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd728f75911f4a72a09ecd0f132bebe3 = () => {
-  return '2026-06-05T01:21:46Z';
+const node_6c6bdb0402b54eb7aaf7c2741099b03f = () => {
+  return '2026-06-12T00:25:48Z';
 };
