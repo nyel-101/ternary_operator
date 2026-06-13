9e910053c97afa1977fdb266f9e481a422933f8f
nyel-101 <dandevjr@gmail.com>
2026-06-13 00:29:15 +0000

    perf(worker): update auth tokens

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47e3f9e6735c437d860fb2b4587775ee = () => {
-  return '2026-02-02T13:00:41Z';
+const node_754fd6ef992e490489ff5f8bdfd72163 = () => {
+  return '2026-06-13T00:29:15Z';
 };
